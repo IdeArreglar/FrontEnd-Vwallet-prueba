32b41e4979847bdaef1fd1a46e70fc0055003e96
Arian Tapia <u202116567@upc.edu.pe>
2024-06-12 11:30:08 -0500

    Listar detalle reserva

---
+++ prueba_front/vwalletpruebafront/src/app/components/detallereservas/creaeditadetallereservas/creaeditadetallereservas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creaeditadetallereservas',
+  standalone: true,
+  imports: [],
+  templateUrl: './creaeditadetallereservas.component.html',
+  styleUrl: './creaeditadetallereservas.component.css'
+})
+export class CreaeditadetallereservasComponent {
+
+}
